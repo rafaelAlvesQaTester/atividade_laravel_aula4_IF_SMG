94d8e81d51de7ad9554c3755a67e6264a96de070
rafaelAlvesQaTester <rafaelalvesmz2@gmail.com>
2024-09-01 17:15:58 -0300

    Create Backend Laravel - part2

--- crudlaravel/app/Models/Produto.php
+++ crudlaravel/app/Models/Produto.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [ // algo preenchivel, no caso apenas os dados que estao dentro das aspas, os demais será o laravel quem irá mexer. 
+        'descricao',
+        'qtd',
+        'precoUnitario',
+        'precoVenda'
+    ]
+
 }
